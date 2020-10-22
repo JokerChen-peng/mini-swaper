81728f6a3870cc77598f991640cd32d23b73feab
kungg <1592500694@qq.com>
2020-10-22 10:23:31 +0800

    Updates

---
+++ precache-manifest.7bc5f3891d6946f848fe697434e0d667.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749384538e3bd98a71beb1fafed808be",
+    "url": "/mini-swaper/index.html"
+  },
+  {
+    "revision": "9f7f073becf4c8b42bb5",
+    "url": "/mini-swaper/static/css/main.49f580aa.chunk.css"
+  },
+  {
+    "revision": "1c2837148d39b6f53979",
+    "url": "/mini-swaper/static/js/2.4a551175.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/mini-swaper/static/js/2.4a551175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f7f073becf4c8b42bb5",
+    "url": "/mini-swaper/static/js/main.db6d6446.chunk.js"
+  },
+  {
+    "revision": "8510b3183a7bb93d952e",
+    "url": "/mini-swaper/static/js/runtime-main.92463a9e.js"
+  }
+]);
